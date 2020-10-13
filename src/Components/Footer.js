6020a6f9ea0935e57a90cca63b3ad2d7b6bc48c2
Reese Hudson <david.reese.hudson@gmail.com>
2020-10-13 15:45:54 -0400

    splitting organisms into .js files

---
+++ src/Components/Footer.js
@@ -0,0 +1,26 @@
+    // <!-- Footer with contact Info -->
+    // <footer class="bg-dark py-3 mt-3" style="position: relative; bottom: 0px;">
+    //     <div class="container-fluid">
+    //         <div class="row">
+    //             <div class="col">
+    //                 <h6 class="text-secondary text-center"> Contact Me</h6>
+    //             </div>
+    //         </div>
+    //         <div class="row">
+    //             <div class="col">
+    //                 <ul class="nav list-unstyled justify-content-center">
+    //                     <li class="nav-item px-3"><i class="fas fa-paper-plane text-secondary" style="width:15px"></i><a
+    //                             class="text-secondary pl-2"
+    //                             href="mailto:david.reese.hudson@gmail.com">david.reese.hudson@gmail.com</a></li>
+    //                     <li class="nav-item px-3"><i class="fas fa-code-branch text-secondary" style="width:15px"></i><a
+    //                             class="text-secondary pl-2" href="https://github.com/dreesehudson">Github</a>
+    //                     </li>
+    //                     <li class="nav-item px-3"><i class="fab fa-linkedin text-secondary" style="width:15px"></i><a
+    //                             class="text-secondary pl-2" href="https://linkedin.com/dreesehudson">LinkedIn</a></li>
+    //                 </ul>
+    //             </div>
+    //         </div>
+    //     </div>
+
+
+    // </footer>
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+
+
+//  <nav>
+//      <a>RH</a>
+//      <button> <span class="navbar-toggler-icon"></span> </button>  
+//          <div id="navbarNav">
+//              >>Blog
+//              >>Projects
+//              >>About
+//          </div>
+//  </nav>
